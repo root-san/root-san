c883554b8d7f9b693f69eb4a6c964487b4ddd25c
kyosu-1 <sho013039@gmail.com>
2022-12-15 03:29:58 +0900

    CreateRoomでresponseを正確に

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -13,50 +13,53 @@ type Server struct {
 
 // create room
 // (POST /rooms)
-func (s *Server) CreateRoom(c echo.Context) error {
+func (s *Server) CreateRoom(ec echo.Context) error {
 	req := api.CreateRoomJSONRequestBody{}
-	if err := c.Bind(&req); err != nil {
+	if err := ec.Bind(&req); err != nil {
 		return err
 	}
 	args := parser.ParseCreateRoomJSONRequestBody(req)
 	if err := s.Repo.CreateRoom(args); err != nil {
 		return err
 	}
-	return nil
+	return ec.JSON(200, api.CreateRoomJSONBody{
+		Id: req.Id,
+		Name: req.Name,
+	})
 }
 
 // get room
 // (GET /rooms/{roomId})
-func (s *Server) GetRoom(c echo.Context, roomId string) error {
+func (s *Server) GetRoom(ec echo.Context, roomId string) error {
 	return nil
 }
 
 // add member to room
 // (POST /rooms/{roomId}/member)
-func (s *Server) AddMember(c echo.Context, roomId string) error {
+func (s *Server) AddMember(ec echo.Context, roomId string) error {
 	return nil
 }
 
 // delete member from room
 // (DELETE /rooms/{roomId}/member/{memberId})
-func (s *Server) DeleteMember(c echo.Context, roomId string, memberId string) error {
+func (s *Server) DeleteMember(ec echo.Context, roomId string, memberId string) error {
 	return nil
 }
 
 // add txn to room
 // (POST /rooms/{roomId}/txn)
-func (s *Server) AddTransaction(c echo.Context, roomId string) error {
+func (s *Server) AddTransaction(ec echo.Context, roomId string) error {
 	return nil
 }
 
 // delete txn from room
 // (DELETE /rooms/{roomId}/txn/{txnId})
-func (s *Server) DeleteTransaction(c echo.Context, roomId string, txnId string) error {
+func (s *Server) DeleteTransaction(ec echo.Context, roomId string, txnId string) error {
 	return nil
 }
 
 // edit txn of room
 // (PUT /rooms/{roomId}/txn/{txnId})
-func (s *Server) EditTransaction(c echo.Context, roomId string, txnId string) error {
+func (s *Server) EditTransaction(ec echo.Context, roomId string, txnId string) error {
 	return nil
 }
