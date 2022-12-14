cd8dd7d7b19eea0001e9916e2b0d1d5c1ad685fd
kyosu-1 <sho013039@gmail.com>
2022-12-15 03:38:48 +0900

    add error handling func

---
+++ app/handler/error.go
@@ -0,0 +1,28 @@
+
+package handler
+
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/root-san/root-san/pkg/dberror"
+	"github.com/root-san/root-san/pkg/echoutil"
+)
+
+func catch(ec echo.Context, err error) error {
+	if errors.Is(err, dberror.ErrDuplicateKey) {
+		return ec.JSON(http.StatusBadRequest, map[string]interface{}{
+			"msg":          "already exist",
+			"error_detail": err.Error(),
+		})
+	}
+	if errors.Is(err, sql.ErrNoRows) {
+		return ec.JSON(http.StatusNotFound, map[string]interface{}{
+			"msg":          "not found",
+			"error_detail": err.Error(),
+		})
+	}
+	return echoutil.ErrInternal(ec, err)
+}
