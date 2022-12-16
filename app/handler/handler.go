19b7ffa274afb616f4519b691910805ddec2b582
kyosu-1 <sho013039@gmail.com>
2022-12-17 05:18:02 +0900

    impl AddEvent

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	openapi_types "github.com/deepmap/oapi-codegen/pkg/types"
 	"github.com/labstack/echo/v4"
 	"github.com/root-san/root-san/app/handler/parser"
 	"github.com/root-san/root-san/app/repository"
@@ -32,13 +33,13 @@ func (s *Server) CreateRoom(ec echo.Context) error {
 
 // get room
 // (GET /rooms/{roomId})
-func (s *Server) GetRoom(ec echo.Context, roomId string) error {
+func (s *Server) GetRoom(ec echo.Context, roomId openapi_types.UUID) error {
 	return nil
 }
 
 // add member to room
 // (POST /rooms/{roomId}/member)
-func (s *Server) AddMember(ec echo.Context, roomId string) error {
+func (s *Server) AddMember(ec echo.Context, roomId openapi_types.UUID) error {
 	req := api.AddMemberJSONRequestBody{}
 	if err := ec.Bind(&req); err != nil {
 		return catch(ec, err)
@@ -55,7 +56,7 @@ func (s *Server) AddMember(ec echo.Context, roomId string) error {
 
 // delete member from room
 // (DELETE /rooms/{roomId}/member/{memberId})
-func (s *Server) DeleteMember(ec echo.Context, roomId string, memberId string) error {
+func (s *Server) DeleteMember(ec echo.Context, roomId openapi_types.UUID, memberId openapi_types.UUID) error {
 	if err := s.Repo.DeleteMember(roomId, memberId); err != nil {
 		return catch(ec, err)
 	}
@@ -64,10 +65,21 @@ func (s *Server) DeleteMember(ec echo.Context, roomId string, memberId string) e
 
 // add event to room
 // (POST /rooms/{roomId}/event)
-func (s *Server) AddEvent(ctx echo.Context, roomId string) error {
-	return nil
+func (s *Server) AddEvent(ctx echo.Context, roomId openapi_types.UUID) error {
+	req := api.AddEventJSONRequestBody{}
+	if err := ctx.Bind(&req); err != nil {
+		return catch(ctx, err)
+	}
+	arg := parser.ParseAddEventJSONRequestBody(req, roomId)
+	if err := s.Repo.CreateEvent(arg); err != nil {
+		return catch(ctx, err)
+	}
+	event, err := s.Repo.GetEvent(arg.Id)
+	if err != nil {
+		return catch(ctx, err)
+	}
+	return ctx.JSON(http.StatusOK, parser.Model{}.Event(event))
 }
-
 
 // delete event from room
 // (DELETE /rooms/{roomId}/event/{eventId})
