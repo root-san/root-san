46a09546a7de3b881642ee6d289a6badcf5ea0d0
kyosu-1 <sho013039@gmail.com>
2022-12-17 05:16:03 +0900

    add parser

--- app/handler/parser/event.go
+++ app/handler/parser/event.go
@@ -2,20 +2,23 @@ package parser
 
 import (
 	"github.com/google/uuid"
+	"github.com/root-san/root-san/app/model"
 	"github.com/root-san/root-san/app/repository"
 	"github.com/root-san/root-san/gen/api"
 )
 
+type Model struct{}
+
 // ParseAddEventJSONRequestBody parses the request body of the AddEvent endpoint
 func ParseAddEventJSONRequestBody(body api.AddEventJSONRequestBody, roomId uuid.UUID) *repository.CreateEventArgs {
 	args := repository.CreateEventArgs{
-		Id:     body.Id,
-		RoomId: roomId,
-		Name:   body.Name,
-		Amount: body.Amount,
+		Id:        body.Id,
+		RoomId:    roomId,
+		Name:      body.Name,
+		Amount:    body.Amount,
 		EventType: string(body.EventType),
-		EventAt: body.EventAt,
-		Txns:    make([]*repository.Transaction, len(body.Txns)),
+		EventAt:   body.EventAt,
+		Txns:      make([]*repository.Transaction, len(body.Txns)),
 	}
 	for i, txn := range body.Txns {
 		args.Txns[i] = &repository.Transaction{
@@ -26,4 +29,25 @@ func ParseAddEventJSONRequestBody(body api.AddEventJSONRequestBody, roomId uuid.
 		}
 	}
 	return &args
+}
+
+func (m Model) Event(e *model.Event) *api.Event {
+	res := &api.Event{
+		Id:        e.Id,
+		Name:      e.Name,
+		Amount:    e.Amount,
+		EventType: api.EventType(e.EventType),
+		EventAt:   e.EventAt,
+		Txns:      make([]api.Txn, len(e.Txns)),
+		CreatedAt: e.CreatedAt,
+	}
+	for i, txn := range e.Txns {
+		res.Txns[i] = api.Txn{
+			Id:       txn.Id,
+			Amount:   txn.Amount,
+			Payer:    txn.Payer,
+			Receiver: txn.Receiver,
+		}
+	}
+	return res
 }
