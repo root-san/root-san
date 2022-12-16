5852c73c0ee65ebd52bfa1283d8eed0108b3ee68
kyosu-1 <sho013039@gmail.com>
2022-12-17 03:03:40 +0900

    fix sql

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -18,23 +18,27 @@ CREATE TABLE IF NOT EXISTS `room_members` (
   FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS `transactions` (
+CREATE TABLE IF NOT EXISTS `events` (
   `id` char(36) NOT NULL,
   `room_id` char(36) NOT NULL,
   `amount` int NOT NULL,
-  `payer_id` char(36) NOT NULL,
-  `paid_at` datetime NOT NULL,
-  `description` varchar(300) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `event_type` varchar(300) NOT NULL,
+  `event_at`   datetime  NOT NULL,
   `created_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
   `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS `transaction_receivers` (
-    `member_id` char(36) NOT NULL,
-    `transaction_id` char(36) NOT NULL,
+CREATE TABLE IF NOT EXISTS `transactions` (
+    `id` char(36) NOT NULL,
+    `event_id` char(36) NOT NULL,
+    `amount` int NOT NULL,
+    `payer_id` char(36) NOT NULL,
+    `receiver_id` char(36) NOT NULL,
     `created_at` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY (`member_id`, `transaction_id`),
-    FOREIGN KEY (`transaction_id`) REFERENCES `transactions`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
