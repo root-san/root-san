81929fa8708400d2eb04c7b6306be286f5919b09
kyosu-1 <sho013039@gmail.com>
2022-12-17 04:00:06 +0900

    add model

---
+++ app/model/event.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"github.com/google/uuid"
+)
+
+type Event struct {
+	Id        uuid.UUID
+	Name      string
+	Amount    int
+	eventType EventType
+	txns      []*Transaction
+}
+
+type EventType string
+
+const (
+	EventTypeOuter EventType = "outer"
+	EventTypeInner EventType = "inner"
+)
+
+type Transaction struct {
+	Id       uuid.UUID
+	Amount   int
+	Payer    uuid.UUID
+	Receiver uuid.UUID
+}
--- app/model/room.go
+++ app/model/room.go
@@ -1,1 +1,41 @@
 package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Room struct {
+	Id        uuid.UUID
+	Name      string
+	CreatedAt time.Time
+}
+
+type Member struct {
+	Id   uuid.UUID
+	Name string
+}
+
+type RoomDetail struct {
+	Id        uuid.UUID
+	Name      string
+	CreatedAt time.Time
+	Members   []*Member
+	Events    []*Event
+}
+
+func NewRoomDetails(room *Room, members []*Member, events []*Event) *RoomDetail {
+	return &RoomDetail{
+		Id:        room.Id,
+		Name:      room.Name,
+		CreatedAt: room.CreatedAt,
+		Members:   members,
+		Events:    events,
+	}
+}
+
+type Result struct {
+	Amount   int
+	Payer    uuid.UUID
+	Receiver uuid.UUID
+}
