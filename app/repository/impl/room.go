c185e9571a5f111d1d0184583949c1310e0cadf7
kyosu-1 <sho013039@gmail.com>
2022-12-17 04:17:42 +0900

    impl GetRoom

--- app/repository/impl/room.go
+++ app/repository/impl/room.go
@@ -1,6 +1,8 @@
 package impl
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/root-san/root-san/app/model"
 	"github.com/root-san/root-san/app/repository"
@@ -15,7 +17,22 @@ func (r *Repository) CreateRoom(args *repository.CreateRoomArgs) error {
 }
 
 func (r *Repository) GetRoom(roomId uuid.UUID) (*model.Room, error) {
-	return nil, nil
+	var room Room
+	err := r.db.Get(&room, "SELECT * FROM rooms WHERE id = ?", roomId)
+	if err != nil {
+		return nil, err
+	}
+	return &model.Room{
+		Id:        room.id,
+		Name:      room.name,
+		CreatedAt: room.createdAt,
+	}, nil
+}
+
+type Room struct {
+	id        uuid.UUID `db:"id"`
+	name      string    `db:"name"`
+	createdAt time.Time `db:"created_at"`
 }
 
 func (r *Repository) GetRoomMembers(roomId uuid.UUID) ([]*model.Member, error) {
