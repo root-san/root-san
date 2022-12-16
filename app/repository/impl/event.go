fa13d56bb71c97d854d032b33819a6629c08416f
kyosu-1 <sho013039@gmail.com>
2022-12-17 04:30:45 +0900

    impl GetEvent

--- app/repository/impl/event.go
+++ app/repository/impl/event.go
@@ -1,6 +1,8 @@
 package impl
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/root-san/root-san/app/model"
 	"github.com/root-san/root-san/app/repository"
@@ -26,7 +28,51 @@ func (r *Repository) CreateEvent(args *repository.CreateEventArgs) error {
 }
 
 func (r *Repository) GetEvent(eventId uuid.UUID) (*model.Event, error) {
-	return nil, nil
+	var eve event
+	err := r.db.Get(eve, "SELECT * FROM events WHERE id = ?", eventId)
+	if err != nil {
+		return nil, err
+	}
+	// eventIdを元にtransactionを取得
+	var txns []*transaction
+	err = r.db.Select(&txns, "SELECT * FROM transactions WHERE event_id = ?", eventId)
+	if err != nil {
+		return nil, err
+	}
+	res := &model.Event{
+		Id:        eve.id,
+		Name:      eve.name,
+		Amount:    eve.amount,
+		EventType: model.EventType(eve.eventType),
+		EventAt:   eve.eventAt,
+		Txns:      make([]*model.Transaction, len(txns)),
+		CreatedAt: eve.createdAt,
+	}
+	for i, txn := range txns {
+		res.Txns[i] = &model.Transaction{
+			Id:       txn.id,
+			Amount:   txn.amount,
+			Payer:    txn.payer,
+			Receiver: txn.receiver,
+		}
+	}
+	return res, nil
+}
+
+type event struct {
+	id        uuid.UUID `db:"id"`
+	name      string    `db:"name"`
+	amount    int       `db:"amount"`
+	eventType string    `db:"event_type"`
+	eventAt   time.Time `db:"event_at"`
+	createdAt time.Time `db:"created_at"`
+}
+
+type transaction struct {
+	id       uuid.UUID `db:"id"`
+	amount   int       `db:"amount"`
+	payer    uuid.UUID `db:"payer"`
+	receiver uuid.UUID `db:"receiver"`
 }
 
 func (r *Repository) UpdateEvent(args *repository.UpdateEventArgs) error {
