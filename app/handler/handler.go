0aea096f241b742a0422567b9d6ae21cbae53874
kyosu-1 <sho013039@gmail.com>
2022-12-17 03:06:51 +0900

    初期化

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -34,28 +33,7 @@ func (s *Server) CreateRoom(ec echo.Context) error {
 // get room
 // (GET /rooms/{roomId})
 func (s *Server) GetRoom(ec echo.Context, roomId string) error {
-	room, err := s.Repo.GetRoom(roomId)
-	if err != nil {
-		return catch(ec, err)
-	}
-	parseMember, err := parser.ParseMemberIdNameArgsToMember(room.Members)
-	if err != nil {
-		return catch(ec, err)
-	}
-	parseResult := parser.ParseResultArgsToResult(room.Results)
-	parseTxn, err := parser.ParseTxnArgsToTxn(room.Txns)
-	if err != nil {
-		return catch(ec, err)
-	}
-	log.Print("aa")
-	return ec.JSON(http.StatusOK, api.RoomDetails{
-		CreatedAt: *room.CreatedAt,
-		Id:        room.Id,
-		Members:   *parseMember,
-		Name:      *room.Name,
-		Results:   *parseResult,
-		Txns:      *parseTxn,
-	})
+	return nil
 }
 
 // add member to room
@@ -84,57 +62,21 @@ func (s *Server) DeleteMember(ec echo.Context, roomId string, memberId string) e
 	return ec.NoContent(http.StatusNoContent)
 }
 
-// add txn to room
-// (POST /rooms/{roomId}/txn)
-func (s *Server) AddTransaction(ec echo.Context, roomId string) error {
-	req := api.AddTransactionJSONRequestBody{}
-	if err := ec.Bind(&req); err != nil {
-		return catch(ec, err)
-	}
-	arg := parser.ParseAddTransactionJSONRequestBody(req, roomId)
-	createdAt, err := s.Repo.AddTransaction(arg)
-	if err != nil {
-		return catch(ec, err)
-	}
-	return ec.JSON(http.StatusOK, api.Txn{
-		Amount:      req.Amount,
-		CreatedAt:   *createdAt,
-		Description: req.Description,
-		Id:          req.Id,
-		PaidAt:      req.PaidAt,
-		Payer:       req.Payer,
-		Receivers:   req.Receivers,
-	})
+// add event to room
+// (POST /rooms/{roomId}/event)
+func (s *Server) AddEvent(ctx echo.Context, roomId string) error {
+	return nil
 }
 
-// delete txn from room
-// (DELETE /rooms/{roomId}/txn/{txnId})
-func (s *Server) DeleteTransaction(ec echo.Context, roomId string, txnId string) error {
-	if err := s.Repo.DeleteTransaction(roomId, txnId); err != nil {
-		return catch(ec, err)
-	}
-	return ec.NoContent(http.StatusNoContent)
+
+// delete event from room
+// (DELETE /rooms/{roomId}/event/{eventId})
+func (s *Server) DeleteEvent(ctx echo.Context, roomId string, eventId string) error {
+	return nil
 }
 
-// edit txn of room
-// (PUT /rooms/{roomId}/txn/{txnId})
-func (s *Server) EditTransaction(ec echo.Context, roomId string, txnId string) error {
-	req := api.EditTransactionJSONRequestBody{}
-	if err := ec.Bind(&req); err != nil {
-		return catch(ec, err)
-	}
-	arg := parser.ParseEditTransactionJSONRequestBody(req, roomId, txnId)
-	createdAt, err := s.Repo.EditTransaction(arg)
-	if err != nil {
-		return catch(ec, err)
-	}
-	return ec.JSON(http.StatusOK, api.Txn{
-		Amount:      req.Amount,
-		CreatedAt:   *createdAt,
-		Description: req.Description,
-		Id:          req.Id,
-		PaidAt:      req.PaidAt,
-		Payer:       req.Payer,
-		Receivers:   req.Receivers,
-	})
+// edit event of room
+// (PUT /rooms/{roomId}/event/{eventId})
+func (s *Server) EditEvent(ctx echo.Context, roomId string, eventId string) error {
+	return nil
 }
