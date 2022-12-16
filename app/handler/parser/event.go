96e64f22945896ab4bcf89e4c9625c276acf89bf
kyosu-1 <sho013039@gmail.com>
2022-12-17 04:45:23 +0900

    add ParseAddEventJSONRequestBody

---
+++ app/handler/parser/event.go
@@ -0,0 +1,27 @@
+package parser
+
+import (
+	"github.com/root-san/root-san/app/repository"
+	"github.com/root-san/root-san/gen/api"
+)
+
+// ParseAddEventJSONRequestBody parses the request body of the AddEvent endpoint
+func ParseAddEventJSONRequestBody(body api.AddEventJSONRequestBody, roomId string) *repository.CreateEventArgs {
+	args := repository.CreateEventArgs{
+		Id:     body.Id,
+		Name:   body.Name,
+		Amount: body.Amount,
+		EventType: string(body.EventType),
+		EventAt: body.EventAt,
+		Txns:    make([]*repository.Transaction, len(body.Txns)),
+	}
+	for i, txn := range body.Txns {
+		args.Txns[i] = &repository.Transaction{
+			Id:       txn.Id,
+			Amount:   txn.Amount,
+			Payer:    txn.Payer,
+			Receiver: txn.Receiver,
+		}
+	}
+	return &args
+}
