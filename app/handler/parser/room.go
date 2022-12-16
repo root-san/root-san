478b8db35eced34dfb390ad79a4c18f3d8b4da6a
kyosu-1 <sho013039@gmail.com>
2022-12-17 07:42:13 +0900

    impl RoomDetail parser

--- app/handler/parser/room.go
+++ app/handler/parser/room.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"github.com/google/uuid"
+	"github.com/root-san/root-san/app/model"
 	"github.com/root-san/root-san/app/repository"
 	"github.com/root-san/root-san/gen/api"
 )
@@ -23,4 +24,48 @@ func ParseAddMemberJSONRequestBody(body api.AddMemberJSONRequestBody, roomId uui
 		Name:   body.Name,
 	}
 	return &args
+}
+
+func (m Model) RoomDetail(r *model.RoomDetails) *api.RoomDetails {
+	res := &api.RoomDetails{
+		Id:        r.Id,
+		CreatedAt: r.CreatedAt,
+		Name:      r.Name,
+		Members:   make([]api.Member, len(r.Members)),
+		Events:    make([]api.Event, len(r.Events)),
+		Results:   make([]api.Result, len(r.Results())),
+	}
+	for i, member := range r.Members {
+		res.Members[i] = api.Member{
+			Id:   member.Id,
+			Name: member.Name,
+		}
+	}
+	for i, event := range r.Events {
+		res.Events[i] = api.Event{
+			Id:        event.Id,
+			Name:      event.Name,
+			Amount:    event.Amount,
+			EventType: api.EventType(event.EventType),
+			EventAt:   event.EventAt,
+			Txns:      make([]api.Txn, len(event.Txns)),
+			CreatedAt: event.CreatedAt,
+		}
+		for j, txn := range event.Txns {
+			res.Events[i].Txns[j] = api.Txn{
+				Id:       txn.Id,
+				Amount:   txn.Amount,
+				Payer:    txn.Payer,
+				Receiver: txn.Receiver,
+			}
+		}
+	}
+	for i, result := range r.Results() {
+		res.Results[i] = api.Result{
+			Payer:    result.Payer,
+			Receiver: result.Receiver,
+			Amount:   result.Amount,
+		}
+	}
+	return res
 }
